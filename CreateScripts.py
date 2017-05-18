071834688c5ea917bbf7ed4ca925d2a971b90ea4
thesim <simon.thevenin@hec.ca>
2017-05-18 15:43:56 -0400

    Evaluate on a different scenario tree, and compute the bounds Take the decision before to know the demand of the next day Change the instance generation method Compute some statistics on the solutions

--- CreateScripts.py
+++ CreateScripts.py
@@ -17,7 +17,7 @@ if __name__=="__main__":
 	#Path to a folder where you want your results to be
 	output_folder=r"/home/thesim/TestFolder/stochasticmrp/"
 
-	for f in ["01"  , "02", "03", "04", "05" ]:# "06", "07", "08", "09", 
+	for f in ["01"]:#   , "02", "03", "04", "05" ]:# "06", "07", "08", "09",
 #			  "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", 
 #			  "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", 
 #			  "30", "31", "32", "33", "34", "35", "36", "37", "38"]:
@@ -43,7 +43,7 @@ if __name__=="__main__":
 #$ -o /home/thesim/outputjob%s%s%s%s%s.txt
 ulimit -v 8000000
 mkdir /tmp/thesim
-python test.py %s 05 %s %s %s %s 10000
+python test.py %s 05 %s %s %s %s 2000
 """ %(f, m, avg, b, nrscenar, f, m, avg, b, nrscenar) )
 
 
@@ -54,7 +54,7 @@ file.write("""
 #!/bin/bash -l
 #
 """ ) 
-for f in ["01"  , "02", "03", "04", "05" ]:# "06", "07", "08", "09", 
+for f in ["01" ]:#  , "02", "03", "04", "05" "06", "07", "08", "09",
 #			  "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", 
 #			  "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", 
 #			  "30", "31", "32", "33", "34", "35", "36", "37", "38"]:
@@ -69,4 +69,4 @@ for f in ["01"  , "02", "03", "04", "05" ]:# "06", "07", "08", "09",
 										scenarset =  [ "1" ]
 										avg = True
 								for nrscenar in scenarset: 
-										file.write("qsub job_%s_%s_%s_%s_%s \n"%(f, m, avg, b, nrscenar )
+										file.write("qsub job_%s_%s_%s_%s_%s \n"%(f, m, avg, b, nrscenar ) )
