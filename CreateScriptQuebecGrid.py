58cda17e8d48bbeb00092033944a4423961abed7
thesim <simon.thevenin@hec.ca>
2017-06-08 11:45:12 -0400

    Add RQMC generator Remove Generate as YQFix

--- CreateScriptQuebecGrid.py
+++ CreateScriptQuebecGrid.py
@@ -17,16 +17,16 @@ if __name__ == "__main__":
     # Path to a folder where you want your results to be
     output_folder = r"/home/thesim/TestFolder/stochasticmrp/"
 
-    for f in ["01"]:  # , "02", "03", "04", "05" ]:# "06", "07", "08", "09",
+    for f in ["01", "02", "03", "04", "05" ]:# "06", "07", "08", "09",
         #			  "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
         #			  "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
         #			  "30", "31", "32", "33", "34", "35", "36", "37", "38"]:
-        for b in ["SlowMoving", "Normal", "Lumpy"]:  # "02", "03", "04", "05", "06", "07", "08", "09", "10"]:
+        for b in ["SlowMoving", "Normal", "Lumpy", "Uniform", "NonStationary"]:  # "02", "03", "04", "05", "06", "07", "08", "09", "10"]:
             for m in ["YFix", "YQFix"]:  # , "YFix", "_Fix" ]:
                 for generation in ["MC", "RQMC"]:
                     scenarset = ["512"]
                     policyset = [ "NearestNeighbor", "Re-solve"]
-                    scenarioasYPset = ["False", "True"]
+                    scenarioasYPset = ["False"]
                     avg = False
                     if m == "YQFix":
                         scenarset = ["2", "4", "8", "50", "100", "200", "500"]
@@ -34,14 +34,14 @@ if __name__ == "__main__":
                         scenarioasYPset =  ["False"  ]
                         scenarioasYP = False
             # if m == "Average":
-            #			scenarset =  [ "1 " ]
+            #			scenarset =  [ "1" ]
 
             #			avg = True
                     for scenarioasYP in scenarioasYPset:
                           for Policy in policyset:  # , "07", "08", "09", "10"]:
                              for nrscenar in scenarset:
-                                qsub_filename = "job_%s_%s_%s_%s_%s_%s_%s_%s" % (
-                                f, m, avg, b, nrscenar, scenarioasYP, Policy, generation)
+                                qsub_filename = "job_%s_%s_%s_%s_%s_%s_%s" % (
+                                f, m, b, nrscenar, scenarioasYP, Policy, generation)
                                 qsub_file = open(qsub_filename, 'w')
                                 qsub_file.write("""
 #!/bin/bash
@@ -62,16 +62,17 @@ file.write("""
 #!/bin/bash -l
 #
 """)
-for f in ["01"]:  # , "02", "03", "04", "05" ]:# "06", "07", "08", "09",
+for f in ["01", "02", "03", "04", "05"]:  # "06", "07", "08", "09",
     #			  "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
     #			  "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
     #			  "30", "31", "32", "33", "34", "35", "36", "37", "38"]:
-    for b in ["SlowMoving", "Normal", "Lumpy"]:  # "02", "03", "04", "05", "06", "07", "08", "09", "10"]:
+    for b in ["SlowMoving", "Normal", "Lumpy", "Uniform",
+              "NonStationary"]:  # "02", "03", "04", "05", "06", "07", "08", "09", "10"]:
         for m in ["YFix", "YQFix"]:  # , "YFix", "_Fix" ]:
             for generation in ["MC", "RQMC"]:
                 scenarset = ["512"]
                 policyset = ["NearestNeighbor", "Re-solve"]
-                scenarioasYPset = ["False", "True"]
+                scenarioasYPset = ["False"]
                 avg = False
                 if m == "YQFix":
                     scenarset = ["2", "4", "8", "50", "100", "200", "500"]
