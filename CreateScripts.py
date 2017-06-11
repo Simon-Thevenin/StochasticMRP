c72ed87f4a17414ba6d88dbb4668903215319818
thesim <simon.thevenin@hec.ca>
2017-06-11 17:10:37 -0400

    Solve some bugs

--- CreateScripts.py
+++ CreateScripts.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
 #$ -cwd
 #$ -q idra
 #$ -j y
-#$ -o /home/thesim/outputjob%s%s%s%s%s%s%s.txt
+#$ -o /home/thesim/outputjob%s%s%s%s%s%s%s%s.txt
 ulimit -v 16000000
 mkdir /tmp/thesim
 python test.py %s 05 %s %s %s %s %s %s %s 500
