46f134616adde9b1f68eb195360cf0ba01e657c3
thesim <simon.thevenin@hec.ca>
2017-06-15 10:43:03 -0400

    Evaluate in parallel

--- test.py
+++ test.py
@@ -585,7 +585,7 @@ if __name__ == "__main__":
         PrintScenarios = False
 
         #Instance.DefineAsSuperSmallIntance()
-        #Instance.ReadInstanceFromExelFile( InstanceName,  Distribution )
+        Instance.ReadInstanceFromExelFile( InstanceName,  Distribution )
        # for InstanceName in ["01" ]:#, "02", "03", "04", "05"]:  # "06", "07", "08", "09",
             #			  "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
             #			  "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
