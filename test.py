25a91ca1d24eb1c716afc13833968159a4f25829
thesim <simon.thevenin@hec.ca>
2017-07-07 17:53:39 -0400

    Change instance generation

--- test.py
+++ test.py
@@ -577,7 +577,7 @@ def RunEvaluation(  ):
         if Model == Constants.ModelYQFix or Model == Constants.Average:
                 policyset = ["Fix"]
         for policy in policyset:
-                jobname = "job_evaluate_%s_%s_%s_%s_%s_%s_%s_s" % (
+                jobname = "job_evaluate_%s_%s_%s_%s_%s_%s_%s_%s" % (
                     TestIdentifier[0],  TestIdentifier[1],  TestIdentifier[2],  TestIdentifier[5], TestIdentifier[4], TestIdentifier[3],  policy, SeedIndex)
                 subprocess.call( ["qsub", jobname]  )
 #This function runs the evaluation jobs when the method is solved for the 5 seed:
