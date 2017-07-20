14993df3e48f70c24f52c2acb108b0b92a0d7838
thesim <simon.thevenin@hec.ca>
2017-07-20 16:26:31 -0400

    modify create script

--- CreateScriptQuebecGrid.py
+++ CreateScriptQuebecGrid.py
@@ -8,66 +8,63 @@ import subprocess
 if __name__ == "__main__":
 
      #for instance in ["00", "01", "02", "03", "04", "05", "01_LTH", "02_LTH", "03_LTH", "04_LTH", "05_LTH" ]:
-    for instance in ["00", "01", "01_LTH" ]:
-        distributionset = [ "NonStationary"]
-        #distributionset = ["SlowMoving", "Normal", "Lumpy", "NonStationary"]
-        if instance == "00":
-            distributionset = ["Binomial"]
-        if instance == "01":
-            distributionset = [ "Uniform", "NonStationary"]
-       #     distributionset = ["SlowMoving", "Normal", "Lumpy", "Uniform", "NonStationary"]
-        for distribution in distributionset:
-            #model = "YFix"
-            #nrscenar = 500
-            #generation = "RQMC"
-            modelset = ["YFix", "YQFix", "Average"]
+    for instance in ["00", "01", "01_LTH"]:
+         distributionset = ["NonStationary"]
+         # distributionset = ["SlowMoving", "Normal", "Lumpy", "NonStationary"]
+         if instance == "00":
+             distributionset = ["Binomial"]
+         if instance == "01":
+             distributionset = ["Uniform", "NonStationary"]
+             #     distributionset = ["SlowMoving", "Normal", "Lumpy", "Uniform", "NonStationary"]
+         for distribution in distributionset:
+             # model = "YFix"
+             # nrscenar = 500
+             # generation = "RQMC"
+             modelset = ["YFix", "YQFix", "Average"]
 
-
-
-            for model in modelset:
+             for model in modelset:
                  generationset = ["MC", "RQMC"]
 
+                 scenarset = ["512"]
 
-                 scenarset = [ "512" ]
-
-                 policyset = [ "S","NNS", "NND", "NNDAC", "NNSAC", "Re-solve"]
+                 policyset = ["S", "NNS", "NND", "NNDAC", "NNSAC", "Re-solve"]
                  methodset = ["MIP"]
                  avg = False
                  if model == "YFix":
                      scenarset = ["512"]
                      if instance in ["00", "01", "02", "03", "04", "05"]:
-                         methodset = ["MIP", "SDDP" ]
+                         methodset = ["MIP", "SDDP"]
                      else:
-                         methodset = ["MIP"]
+                         methodset = ["SDDP"]
 
                  if model == "YQFix":
-                     #scenarset = [ "1000" ]
+                     # scenarset = [ "1000" ]
                      scenarset = ["2", "4", "8", "50", "100", "200", "500", "1000"]
-                     policyset = [ "Fix" ]
+                     policyset = ["Fix"]
 
                  if model == "Average":
-                     scenarset =  [ "1" ]
+                     scenarset = ["1"]
                      avg = True
                      policyset = ["Fix"]
                      generationset = ["MC"]
 
-
                  for method in methodset:
 
-                     if (instance == "00" or instance == "01") and (model == "YFix" and method == "MIP"):
-                        generationset = ["MC", "RQMC", "all"]
+                     if (instance == "00" or instance == "01") and (model == "YFix" and method == "MIP") and (
+                             distribution == "Binomial" or distribution == "Uniform"):
+                         generationset = ["MC", "RQMC", "all"]
 
                      for generation in generationset:
 
                          if method == "SDDP":
-                             scenarset = ["10",  "50", "100", "200"]
-                             policyset = [ "SDDP" ]
+                             scenarset = ["10", "50", "100", "200"]
+                             policyset = ["SDDP"]
 
                          if model == "YFix" and method == "MIP":
                              scenarset = ["512"]
 
                          for nrscenar in scenarset:
-                              for seed in range( 5 ):
+                             for seed in range(5):
                                     qsub_filename = "job_solve_%s_%s_%s_%s_%s_%s_%s" % (
                                             instance, distribution, model, nrscenar, generation, seed, method  )
                                     qsub_file = open(qsub_filename, 'w')
@@ -136,7 +133,7 @@ python test.py Evaluate %s %s %s %s %s  -s %s -p %s
                      if instance in ["00", "01", "02", "03", "04", "05"]:
                          methodset = ["MIP", "SDDP" ]
                      else:
-                         methodset = ["MIP"]
+                         methodset = ["SDDP"]
 
                  if model == "YQFix":
                      #scenarset = [ "1000" ]
@@ -152,7 +149,7 @@ python test.py Evaluate %s %s %s %s %s  -s %s -p %s
 
                  for method in methodset:
 
-                     if (instance == "00" or instance == "01") and (model == "YFix" and method == "MIP"):
+                     if (instance == "00" or instance == "01") and (model == "YFix" and method == "MIP") and (distribution == "Binomial" or distribution == "Uniform"):
                         generationset = ["MC", "RQMC", "all"]
 
                      for generation in generationset:
