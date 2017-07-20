6d9b8cd9d35f2a19b084ed08b7f858c83850c6d5
thesim <simon.thevenin@hec.ca>
2017-07-20 15:24:50 -0400

    modify create script

--- CreateScriptQuebecGrid.py
+++ CreateScriptQuebecGrid.py
@@ -7,17 +7,7 @@ import subprocess
 
 if __name__ == "__main__":
 
-    # Path to the program you want to run
-    exec_file = r"/home/thesim/TestFolder/stochasticmrp/"
-
-    # Path to a folder containing the data set you want to run your program on.
-    # Your program will be run on every file in the folder but the files whose names start by '.' (hidden files)
-    data_folder = r"/home/isabel/Models/OneT/Data/no_trend"
-
-    # Path to a folder where you want your results to be
-    output_folder = r"/home/thesim/TestFolder/stochasticmrp/"
-
-    #for instance in ["00", "01", "02", "03", "04", "05", "01_LTH", "02_LTH", "03_LTH", "04_LTH", "05_LTH" ]:
+     #for instance in ["00", "01", "02", "03", "04", "05", "01_LTH", "02_LTH", "03_LTH", "04_LTH", "05_LTH" ]:
     for instance in ["00", "01", "01_LTH" ]:
         distributionset = [ "NonStationary"]
         #distributionset = ["SlowMoving", "Normal", "Lumpy", "NonStationary"]
@@ -36,13 +26,15 @@ if __name__ == "__main__":
 
             for model in modelset:
                  generationset = ["MC", "RQMC"]
-                 if instance == "00" or instance == "01":
-                     generationset = ["MC", "RQMC", "all"]
-                 scenarset = [ "512" ]# "10", "50", "100", "500"]
+
+
+                 scenarset = [ "512" ]
+
                  policyset = [ "S","NNS", "NND", "NNDAC", "NNSAC", "Re-solve"]
                  methodset = ["MIP"]
                  avg = False
                  if model == "YFix":
+                     scenarset = ["512"]
                      if instance in ["00", "01", "02", "03", "04", "05"]:
                          methodset = ["MIP", "SDDP" ]
                      else:
@@ -59,11 +51,19 @@ if __name__ == "__main__":
                      policyset = ["Fix"]
                      generationset = ["MC"]
 
-                 for generation in generationset:
-                     for method in methodset:
+
+                 for method in methodset:
+                     if (instance == "00" or instance == "01") and (model == "YFix" and method == "MIP"):
+                        generationset = ["MC", "RQMC", "all"]
+
+                     for generation in generationset:
+
                          if method == "SDDP":
                              scenarset = ["10",  "50", "100", "200", "500", "1000"]
                              policyset = [ "SDDP" ]
+
+                         if model == "YFix":
+                             scenarset = ["512"]
 
                          for nrscenar in scenarset:
                               for seed in range( 5 ):
@@ -98,59 +98,59 @@ python test.py Evaluate %s %s %s %s %s  -s %s -p %s
  """ % (instance, distribution, model, nrscenar, generation, seed, Policy) )
 
 
-# Create the sh file
-filename = "runalljobs.sh"
-file = open(filename, 'w')
-file.write("""
+    # Create the sh file
+    filename = "runalljobs.sh"
+    file = open(filename, 'w')
+    file.write("""
 #!/bin/bash -l
 #
 """)
-for instance in ["00", "01", "01_LTH"]:
-    distributionset = ["NonStationary"]
-    # distributionset = ["SlowMoving", "Normal", "Lumpy", "NonStationary"]
-    if instance == "00":
-        distributionset = ["Binomial"]
-    if instance == "01":
-        distributionset = ["Uniform", "NonStationary"]
-        #     distributionset = ["SlowMoving", "Normal", "Lumpy", "Uniform", "NonStationary"]
-    for distribution in distributionset:
-        # model = "YFix"
-        # nrscenar = 500
-        # generation = "RQMC"
-        modelset = ["YFix", "YQFix", "Average"]
+    for instance in ["00", "01", "01_LTH"]:
+        distributionset = ["NonStationary"]
+        # distributionset = ["SlowMoving", "Normal", "Lumpy", "NonStationary"]
+        if instance == "00":
+            distributionset = ["Binomial"]
+        if instance == "01":
+            distributionset = ["Uniform", "NonStationary"]
+            #     distributionset = ["SlowMoving", "Normal", "Lumpy", "Uniform", "NonStationary"]
+        for distribution in distributionset:
+            # model = "YFix"
+            # nrscenar = 500
+            # generation = "RQMC"
+            modelset = ["YFix", "YQFix", "Average"]
 
-        for model in modelset:
-            generationset = ["MC", "RQMC"]
-            if instance == "00" or instance == "01":
-                generationset = ["MC", "RQMC", "all"]
-            scenarset = ["512"]  # "10", "50", "100", "500"]
-            policyset = ["S", "NNS", "NND", "NNDAC", "NNSAC", "Re-solve"]
-            methodset = ["MIP"]
-            avg = False
-            if model == "YFix":
-                if instance in ["00", "01", "02", "03", "04", "05"]:
-                    methodset = ["MIP", "SDDP"]
-                else:
-                    methodset = ["MIP"]
+            for model in modelset:
+                generationset = ["MC", "RQMC"]
+                if instance == "00" or instance == "01":
+                    generationset = ["MC", "RQMC", "all"]
+                scenarset = ["512"]  # "10", "50", "100", "500"]
+                policyset = ["S", "NNS", "NND", "NNDAC", "NNSAC", "Re-solve"]
+                methodset = ["MIP"]
+                avg = False
+                if model == "YFix":
+                    if instance in ["00", "01", "02", "03", "04", "05"]:
+                        methodset = ["MIP", "SDDP"]
+                    else:
+                        methodset = ["MIP"]
 
-            if model == "YQFix":
-                # scenarset = [ "1000" ]
-                scenarset = ["2", "4", "8", "50", "100", "200", "500", "1000"]
-                policyset = ["Fix"]
+                if model == "YQFix":
+                    # scenarset = [ "1000" ]
+                    scenarset = ["2", "4", "8", "50", "100", "200", "500", "1000"]
+                    policyset = ["Fix"]
 
-            if model == "Average":
-                scenarset = ["1"]
-                avg = True
-                policyset = ["Fix"]
-                generationset = ["MC"]
+                if model == "Average":
+                    scenarset = ["1"]
+                    avg = True
+                    policyset = ["Fix"]
+                    generationset = ["MC"]
 
-            for generation in generationset:
-                for method in methodset:
-                    if method == "SDDP":
-                        scenarset = ["10", "50", "100", "200", "500", "1000"]
-                        policyset = ["SDDP"]
+                for generation in generationset:
+                    for method in methodset:
+                        if method == "SDDP":
+                            scenarset = ["10", "50", "100", "200", "500", "1000"]
+                            policyset = ["SDDP"]
 
-                    for nrscenar in scenarset:
-                        for seed in range(5):
-                            file.write("qsub job_solve_%s_%s_%s_%s_%s_%s_%s \n" % (
-                                            instance, distribution, model, nrscenar, generation, seed, method  ))
+                        for nrscenar in scenarset:
+                            for seed in range(5):
+                                file.write("qsub job_solve_%s_%s_%s_%s_%s_%s_%s \n" % (
+                                                instance, distribution, model, nrscenar, generation, seed, method  ))
