cee52c7e0dfd16798930617f753192d072afddcb
thesim <simon.thevenin@hec.ca>
2017-07-22 10:16:45 -0400

    solve small issue

--- test.py
+++ test.py
@@ -647,7 +647,7 @@ if __name__ == "__main__":
         #
 
 
-        #Instance.ReadInstanceFromExelFile( InstanceName,  Distribution )
+        Instance.ReadInstanceFromExelFile( InstanceName,  Distribution )
         #for InstanceName in ["01", "02", "03", "04", "05"]:  # "06", "07", "08", "09",
             #			  "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
             #			  "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
