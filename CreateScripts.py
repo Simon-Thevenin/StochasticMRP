1a627711248f567f35ef573aa0fe7aeb6ea0c926
thesim <simon.thevenin@hec.ca>
2017-09-04 12:47:42 -0400

    solve problem in create scripts

--- CreateScripts.py
+++ CreateScripts.py
@@ -116,7 +116,8 @@ if __name__ == "__main__":
 #$ -o /home/thesim/outputjob%s%s%s%s%s%s%s.txt
 ulimit -v 16000000
 mkdir /tmp/thesim
-python ../test.py Solve %s %s %s %s %s -s %s  -n 500 -m %s
+cd ..
+python test.py Solve %s %s %s %s %s -s %s  -n 500 -m %s
 """ % ( instance, distribution, model, nrscenar, generation, seed, method,  instance, distribution, model, nrscenar, generation, seed, method  ))
                                     for Policy in policyset:
                                           qsub_filename = "./Jobs/job_evaluate_%s_%s_%s_%s_%s_%s_%s_%s" % (
@@ -131,7 +132,8 @@ python ../test.py Solve %s %s %s %s %s -s %s  -n 500 -m %s
 #$ -o /home/thesim/outputjobevaluate%s%s%s%s%s%s%s.txt
 ulimit -v 16000000
 mkdir /tmp/thesim
-python ../test.py Evaluate %s %s %s %s %s  -s %s -p %s
+cd ..
+python test.py Evaluate %s %s %s %s %s  -s %s -p %s
  """ % (instance, distribution, model, nrscenar, generation, seed, Policy, instance, distribution, model, nrscenar, generation, seed, Policy) )
 
 
