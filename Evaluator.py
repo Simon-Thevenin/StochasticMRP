623b8b2a4e5097e8e021cf39d221159392280e7f
thesim <simon.thevenin@hec.ca>
2017-10-02 10:57:08 -0400

    debug

--- Evaluator.py
+++ Evaluator.py
@@ -266,12 +266,11 @@ class Evaluator:
     def ComputeStatistic(self, Evaluated, nrscenario, testidentifier, evaluateidentificator, KPIStat, nrerror, model  ):
         mean = np.mean(Evaluated)
         variance = math.pow(np.std(Evaluated), 2)
-        K = 1 # len(Evaluated)
+        K =  len(Evaluated)
         M = nrscenario
         variance2 = ((1.0 / K) * sum(  (1.0 / M) * sum(math.pow(Evaluated[k][seed], 2) for seed in range(M)) for k in range(K))) - math.pow(mean,  2)
         covariance = ( ((1.0 / K) * sum(math.pow(sum(Evaluated[k][seed] for seed in range(M)) / M, 2) for k in range(K))) - math.pow( mean, 2))
-        print "math.sqrt(( %r+ (%r * (%r - 1))) / (%r * %r))"%(variance, covariance, M, K, M)
-        term = stats.norm.ppf(1 - 0.05) * math.sqrt((variance + (covariance * (M - 1))) / (K * M))
+        term = 0# stats.norm.ppf(1 - 0.05) * math.sqrt((variance + (covariance * (M - 1))) / (K * M))
         LB = mean - term
         UB = mean + term
         d = datetime.now()
