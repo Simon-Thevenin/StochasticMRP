61f71f4577e362a0ce657d588de4969f82e6e064
thesim <simon.thevenin@hec.ca>
2017-11-26 11:49:41 -0500

    Solve Issue YQFix Re-solve

--- MIPSolver.py
+++ MIPSolver.py
@@ -422,7 +422,7 @@ class MIPSolver(object):
                         upperbound[self.GetIndexQuantityVariable(p,t,w)] =  max((setup) * self.M,0.0)
 
 
-        self.Cplex.variables.add(obj= variableproductioncosts, #[0.0] * nrquantityvariable,#
+        self.Cplex.variables.add(obj= [0.0] * nrquantityvariable,#variableproductioncosts, #
                                 lb=[0.0] * nrquantityvariable,
                                 ub= upperbound)
 
