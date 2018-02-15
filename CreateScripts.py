22f20bbf1709ab890716a33b8d95444f407086b8
thesim <simon.thevenin@hec.ca>
2018-02-15 11:28:14 -0500

    Merge branch 'develop' of https://bitbucket.org/Simon_Thevenin/stochasticmrp into develop
    Conflicts:
    	Instances/G0044214_NonStationary_b2_fe25_en_rk25_ll0_l20_HFalse_c0.xlsx
    	Instances/G0044432_NonStationary_b2_fe25_en_rk50_ll0_l20_HFalse_c0.xlsx
    	Instances/G0047224_SlowMoving_b4_fe0_el_rk0_ll1_l40_HFalse_c0.xlsx
    	Instances/G5044325_NonStationary_b2_fe25_en_rk50_ll0_l20_HFalse_c0.xlsx
    	Instances/G5044425_Lumpy_b4_fe0_el_rk0_ll1_l40_HFalse_c0.xlsx
    	Instances/G5047313_NonStationary_b4_fe25_el_rk50_ll1_l40_HFalse_c0.xlsx
    	Instances/G5047534_NonStationary_b4_fe25_el_rk50_ll1_l40_HFalse_c0.xlsx
    	Instances/K0011313_SlowMoving_b2_fe0_en_rk0_ll1_l20_HFalse_c0.xlsx
    	Instances/K0014111_Lumpy_b2_fe0_en_rk0_ll0_l20_HFalse_c0.xlsx
    	Instances/K5014225_NonStationary_b2_fe25_el_rk25_ll1_l20_HFalse_c0.xlsx
    	Instances/K5017331_Lumpy_b2_fe0_el_rk0_ll1_l20_HFalse_c0.xlsx
    	Instances/K5017522_NonStationary_b2_fe25_en_rk50_ll1_l20_HFalse_c0.xlsx

--- CreateScripts.py
+++ CreateScripts.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     Nrseed = 1
     if sys.argv[1] == "preliminary":
         modelset = [  "AverageSS", "AverageSSGrave", "Average",  "L4L", "EOQ", "POQ", "SilverMeal",  "YQFix",  "YFix", "HeuristicYFix", "SFix" ]
-        modelset = ["YFix"]
+        modelset = ["AverageSSGrave"]
         nrcenarioysfix= [ "10", "25", "50", "100"]
         nrcenarioyfqix = [ "10", "25", "50", "100", "200", "500", "1000"]
         nrcenarioyqfix = ["10", "25", "50", "100"]
